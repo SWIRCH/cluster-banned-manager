888a955c1dbaf8c53b5eecf6f87d886891031e53
SWIRCH <sw1rchir@gmail.com>
2026-09-01 02:51:36 +0300

    update: config file

--- docs/src/utils/config.ts
+++ docs/src/utils/config.ts
@@ -1,6 +1,10 @@
+const url = "https://github.com/SWIRCH/cluster-banned-manager";
+
 export const config = {
-  GITHUB_URL: "https://github.com/SWIRCH/cluster-banned-manager",
-  RELEASE_URL: "https://github.com/SWIRCH/cluster-banned-manager/releases",
+  GITHUB_URL: url,
+  RELEASE_URL: url + "/releases",
+  ISSUES_URL: url + "/issues",
+  CREATE_ISSUES_URL: url + "/issues/new",
   BASE_URL: "/cluster-banned-manager",
   DOCS_URL: "/cluster-banned-manager/docs/quickstart",
 };
