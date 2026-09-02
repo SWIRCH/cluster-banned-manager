6569df3f0391737a9afff97e1d94e10c2562a8b6
SWIRCH <sw1rchir@gmail.com>
2026-09-02 05:50:13 +0300

    docs: update site config, sitemap filtering

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,11 +1,12 @@
-import { unified } from "@astrojs/markdown-remark"
-import mdx from "@astrojs/mdx"
-import react from "@astrojs/react"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
-import path from "path"
-import remarkEmoji from "remark-emoji"
-import remarkGithubAlerts from "remark-github-alerts"
+import { unified } from "@astrojs/markdown-remark";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import path from "path";
+import remarkEmoji from "remark-emoji";
+import remarkGithubAlerts from "remark-github-alerts";
+import sitemap from "@astrojs/sitemap";
 
 const unifiedProcessor = unified({
   syntaxHighlight: "prism",
@@ -14,7 +15,27 @@ const unifiedProcessor = unified({
 });
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    mdx(),
+    sitemap({
+      filter: (page) => {
+        if (page === "https://swirch.github.io/cluster-banned-manager/") {
+          return true;
+        }
+
+        return page.includes("/cluster-banned-manager/docs/ru/") || page.includes("/cluster-banned-manager/docs/en/");
+      },
+
+      i18n: {
+        defaultLocale: "ru",
+        locales: {
+          ru: "ru",
+          en: "en",
+        },
+      },
+    }),
+  ],
   site: "https://swirch.github.io",
   base: "/cluster-banned-manager",
 
